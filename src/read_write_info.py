cad4a7382830e5285bf977a1da06466c6dd1b504
Anna Li <annajli@berkeley.edu>
2024-06-07 09:34:29 -0700

    adding template read write file

---
+++ src/read_write_info.py
@@ -0,0 +1,7 @@
+#### For cluster dev ####
+# READ_PATH_PREFIX = "/n/holyscratch01/doshi-velez_lab/Users/atrella/"
+# WRITE_PATH_PREFIX = "/n/holyscratch01/doshi-velez_lab/Users/atrella/"
+
+#### For local dev ####
+READ_PATH_PREFIX = ""
+WRITE_PATH_PREFIX = ""
